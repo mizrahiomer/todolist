d65dc1950230868fbef7e27602ded07f0c43ec74
Omer <omermiz1604gmail.com>
2019-12-06 13:13:17 +0200

    Updates

---
+++ precache-manifest.f01b3fd447d8204e04ebc916ccd70154.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b64a4bdc89011e4f7745164234f7981",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "17cd38081c0fdf362ed7",
+    "url": "/todolist/static/js/2.fa20249c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todolist/static/js/2.fa20249c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f7a8b7457f77a144685",
+    "url": "/todolist/static/js/main.0e719942.chunk.js"
+  },
+  {
+    "revision": "b5631cdcdd14849564a7",
+    "url": "/todolist/static/js/runtime-main.75b5d516.js"
+  }
+]);
