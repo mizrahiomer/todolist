ae1e6c87b8b3ccdda2d863cc9811f8eefc06762b
Omer <omermiz1604gmail.com>
2019-12-08 16:43:44 +0200

    update and design changes

--- src/components/Task.js
+++ src/components/Task.js
@@ -5,14 +5,24 @@ const Task = (props) => {
     const border = props.isComplete ? 'border-success' : 'border-danger';
     const completeBtn = props.isComplete ? 'far fa-2x fa-check-circle text-success mx-2' : 'far fa-2x fa-circle mx-2';
     const completeTxt = props.isComplete ? cls.complete : 'd-inline'
+    const taskContent = props.isEditing ?
+        <form onSubmit={props.editSubmit}>
+            <input autoFocus type='text' value={props.contentEdit} className='d-inline form-control p-2' onChange={props.onEdit} onBlur={props.editSubmit} />
+        </form>
+        : <h2 className={completeTxt}>{props.content}</h2>;
+    const editBtn = !props.isEditing ?
+        <button className='btn btn-primary mx-1' onClick={props.edit}><i className="fas fa-edit"></i></button> :
+        <button className='btn btn-success mx-1' onClick={props.editSubmit}><i className="fas fa-check"></i></button>
     return (
-        <li className={cls.task + ' col-sm-6 my-2'} onDragOver={props.dragOver}>
-            <div className={'bg-light shadow p-2 border rounded ' + border} draggable onDragStart={props.dragStart} onDragEnd={props.dragEnd}>
-                <i onClick={props.complete} className={completeBtn}></i>
-                <h4 className={completeTxt}>{props.name}</h4>
-                <p className='text-secondary'>{props.date}</p>
-                <button className='btn btn-danger mx-1' onClick={props.delete}>Delete</button>
-                <button className='btn btn-primary mx-1'>Edit</button>
+        <li className={cls.task + ' card p-2 m-2 col-sm-6 border ' + border} onDragOver={props.dragOver}>
+            <div draggable onDragStart={props.dragStart} onDragEnd={props.dragEnd}>
+                {!props.isEditing ? <i onClick={props.complete} className={completeBtn}></i> : null}
+                {taskContent}
+                <p className='text-muted h6'>{props.date}</p>
+                <div className='text-right'>
+                    <button className='btn btn-danger mx-1' onClick={props.delete}><i className="fas fa-trash-alt"></i></button>
+                    {editBtn}
+                </div>
             </div>
         </li>
     )
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -5,23 +5,46 @@ class Tasks extends Component {
     state = {
         newTask: '',
         tasks: [],
-        show: 'all'
+        editTask: '',
+        show: 'all',
+        error: true
     }
+
     onChangeHandler = e => {
-        console.log(e.target.value);
-        this.setState({ newTask: e.target.value });
+        this.setState({ newTask: e.target.value, error: e.target.value.length > 0 ? false : true });
+    }
+    onEditHandler = e => {
+        console.log(e.target.value)
+        this.setState({ editTask: e.target.value })
     }
     addTask = (event) => {
         event.preventDefault();
         const newTask = this.state.newTask;
         const options = { year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' };
         const date = new Date().toLocaleDateString('en-US', options);
-        const id = this.state.tasks.length + 1 + newTask;
-        const task = { task: newTask, date: date, id: id, isComplete: false };
+        const id = this.state.tasks.length + newTask + date;
+        const task = { content: newTask, date: date, id: id, isComplete: false, isEditing: false };
         this.setState({
             tasks: [...this.state.tasks, task],
             newTask: '',
+            show: 'all',
+            error: true
         });
+    }
+    editTaskSubmit = (event, id) => {
+        event.preventDefault();
+        const tasks = [...this.state.tasks];
+        const index = tasks.findIndex(task => task.id === id);
+        const newTask = this.state.editTask;
+        const options = { year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+        const newDate = new Date().toLocaleDateString('en-US', options);
+        const newId = this.state.tasks.length + 1 + newTask + newDate;
+        tasks[index].content = newTask;
+        tasks[index].id = newId;
+        tasks[index].date = newDate;
+        tasks[index].isEditing = false;
+        this.setState({ tasks: tasks })
+
     }
     deleteTask = (id) => {
         this.setState(prevState => {
@@ -29,6 +52,15 @@ class Tasks extends Component {
                 tasks: prevState.tasks.filter(task => task.id !== id)
             };
         })
+    }
+    deleteAllTasks = () => {
+        this.setState({ tasks: [] })
+    }
+    editTask = (id) => {
+        const tasks = [...this.state.tasks];
+        const index = tasks.findIndex(task => task.id === id);
+        tasks[index].isEditing = !tasks[index].isEditing;
+        this.setState({ tasks: tasks, editTask: tasks[index].content });
     }
     completeTask = (id) => {
         const tasks = [...this.state.tasks];
@@ -75,39 +107,51 @@ class Tasks extends Component {
                 break;
             default: return;
         }
-        const tasks = filterTasks.map((task, i) => (
-            <Task
-                key={task.id}
-                name={task.task}
-                date={task.date}
-                delete={() => this.deleteTask(task.id)}
-                isComplete={task.isComplete}
-                complete={() => this.completeTask(task.id)}
-                dragStart={e => this.dragStart(e, i)}
-                dragOver={() => this.dragOver(i)}
-                dragEnd={this.dragEnd}
-            >
-            </Task>
+        let tasks = null;
+        if (filterTasks.length === 0) {
+            tasks = <p>There are no todos yet</p>;
+        } else {
+            tasks = filterTasks.map((task, i) => (
+                <Task
+                    key={task.id}
+                    content={task.content}
+                    contentEdit={this.state.editTask}
+                    date={task.date}
+                    delete={() => this.deleteTask(task.id)}
+                    edit={() => this.editTask(task.id)}
+                    onEdit={(e) => this.onEditHandler(e)}
+                    editSubmit={(e) => this.editTaskSubmit(e, task.id)}
+                    isComplete={task.isComplete}
+                    isEditing={task.isEditing}
+                    complete={() => this.completeTask(task.id)}
+                    dragStart={e => this.dragStart(e, i)}
+                    dragOver={() => this.dragOver(i)}
+                    dragEnd={this.dragEnd}
+                >
+                </Task>
 
-        ));
-        const btnCls = 'btn shadow btn-sm m-2 '
+            ));
+        }
         return (
             <div className='container'>
-                <form className='col-sm-8' onSubmit={this.addTask}>
-                    <input placeholder='What needs to be done?' value={this.state.newTask} className='form-control shadow m-2 p-2' onChange={this.onChangeHandler} />
+                <form className='row' onSubmit={this.addTask}>
+                    <input autoFocus type='text' placeholder='What needs to be done?' value={this.state.newTask} className='form-control d-inline col-sm-7 shadow m-2 p-2' onChange={this.onChangeHandler} />
+                    <button disabled={this.state.error ? true : false} type='submit' className='btn text-primary shadow-none'><i className="fas fa-2x fa-plus"></i></button>
+                    <button type='button' onClick={this.deleteAllTasks} disabled={this.state.tasks.length === 0 ? true : false} className='btn text-success shadow-none'><i class="fas fa-2x fa-trash-alt"></i></button>
                 </form>
+                {this.state.error ? <p className=' col-sm-2 text-danger'>Write Something...</p> : null}
+                {/* <button className='btn btn-primary m-4' onClick={() => console.log(this.state)}></button> */}
                 {this.state.tasks.length > 0 ?
                     <div className='container-fluid'>
-                        <button className={btnCls + 'btn-secondary'} onClick={() => this.showAll()}>All</button>
-                        <button className={btnCls + 'btn-danger'} onClick={() => this.showActive()}>Active</button>
-                        <button className={btnCls + 'btn-success'} onClick={() => this.showCompleted()}>Completed</button>
+                        <button className={this.state.show === 'all' ? ' btn btn-secondary m-2 btn-sm' : ' btn btn-outline-secondary m-2 btn-sm'} onClick={() => this.showAll()}>All</button>
+                        <button className={this.state.show === 'active' ? ' btn btn-danger m-2 btn-sm' : ' btn btn-outline-danger m-2 btn-sm'} onClick={() => this.showActive()}>Active</button>
+                        <button className={this.state.show === 'completed' ? ' btn btn-success m-2 btn-sm' : ' btn btn-outline-success m-2 btn-sm'} onClick={() => this.showCompleted()}>Completed</button>
                     </div> : null}
                 <ul>
                     {tasks}
                 </ul>
+
             </div>
-
-
         )
 
     }
